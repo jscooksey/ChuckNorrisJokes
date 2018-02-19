2d04f007b41b313baa310e40461a52401e67c23f
Justin Cooksey <justin@cooksey.com.au>
2018-02-19 22:23:05 +1100

    Chuck Norris Jokes
    Grbas Chuck Norris joakes off the internet and displays them on the phone

---
+++ MainScreen/MainScreenPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MainScreen.Model;
+using Xamarin.Forms;
+
+namespace MainScreen
+{
+    public partial class MainScreenPage : ContentPage
+    {
+        public MainScreenPage()
+        {
+            InitializeComponent();
+        }
+
+        async void getJoke_Clicked(object sender, System.EventArgs e)
+        {
+            jokeLabel.TextColor = Color.Fuchsia;
+            jokeLabel.Text = "...geting a joke...";
+            var strJoke = await Joke.GetJoke();
+            jokeLabel.TextColor = Color.Black;
+            jokeLabel.Text = strJoke;
+
+        }
+    }
+}
