58b8b1fe55ba95a7bb405241fac7a0d2c871998a
Justin Cooksey <justin@cooksey.com.au>
2018-02-20 19:10:20 +1100

    Chuck Norris Jokes
    App to get Chuck Norris jokes from online

---
+++ WindowsWPF/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MainScreen.Model;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WindowsWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void getJokeButton_Clicked(object sender, RoutedEventArgs e)
+        {
+            jokeTextBox.Foreground = Brushes.Fuchsia;
+            jokeTextBox.TextAlignment = TextAlignment.Center;
+            jokeTextBox.Text = "...geting a joke...";
+
+            var strJoke = await Joke.GetJoke();
+
+            jokeTextBox.Foreground = Brushes.Black;
+            jokeTextBox.TextAlignment = TextAlignment.Justify;
+            jokeTextBox.Text = strJoke;
+        }
+    }
+}
---
+++ WindowsWPF/Model/Joke.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MainScreen.Model
+{
+    public class Joke
+    {
+        
+        public object category { get; set; }
+        public string icon_url { get; set; }
+        public string id { get; set; }
+        public string url { get; set; }
+        public string value { get; set; }
+
+        private const string queryUrl = "https://api.chucknorris.io/jokes/random";
+
+        public async static Task<string> GetJoke()
+        {
+            string strJoke;
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(queryUrl);
+                var json = await response.Content.ReadAsStringAsync();
+
+                var chuckJoke = JsonConvert.DeserializeObject<Joke>(json);
+
+                strJoke = chuckJoke.value;
+            }
+
+
+            return strJoke;
+        }
+    
+    }
+}
